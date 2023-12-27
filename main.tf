d8e1f29b3fc6ec2934625055310d5cf3ebdb08eb
Sebastian Daberdaku <sebastian.daberdaku@cardoai.com>
2023-12-27 21:54:06 +0100

    remote_state_bootstrap module now uses fixed tag version

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "remote_state_bootstrap" {
-  source = "git@github.com:sebastiandaberdaku/terraform-modules.git//aws-terraform-remote-state"
+  source = "git@github.com:sebastiandaberdaku/terraform-modules.git//aws-terraform-remote-state?ref=aws-terraform-remote-state/v1.0.0"
 
   company     = local.company
   environment = var.environment
